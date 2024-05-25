531415870fc5df1a8384497d697626201f8bc25f
graef <simonjn45@gmail.com>
2024-05-25 14:07:38 +0200

    Added some generator code, html and css

--- colour.js
+++ colour.js
@@ -1,29 +1,28 @@
 /**
  * Code based on:
  * https://matthewstrom.com/writing/generating-color-palettes/
- *
+ * https://bottosson.github.io/misc/colorpicker/
  */
 
 import {
   parse,
   convertOkhslToOklab,
   convertOklabToRgb,
-  formatHex,
-  // formatRgb,
+  formatRgb,
 } from "https://deno.land/x/culori@v4.0.1/index.js";
 
 /**
  *
  * @param {number} scaleNumber
  * @param {number} maxScaleNumber
- * @returns {number}
+ * @returns {number} - a number that is between 0 and 1
  */
 const normalizeScaleNumber = (scaleNumber, maxScaleNumber) =>
   scaleNumber / maxScaleNumber;
 
 /**
  *
- * @param {number} scaleValue
+ * @param {number} scaleValue - Must be between 0 and 1
  * @param {number} baseHue
  * @returns {number}
  */
@@ -31,31 +30,28 @@ const computeScaleHue = (scaleValue, baseHue) => baseHue + 5 * (1 - scaleValue);
 
 /**
  *
- * @param {number} scaleValue
+ * @param {number} scaleValue - Must be between 0 and 1
  * @param {number} minChroma
  * @param {number} maxChroma
  * @returns {number}
  */
 const computeScaleChroma = (scaleValue, minChroma, maxChroma) => {
   const chromaDifference = maxChroma - minChroma;
-  // console.log(`chromaDifference: ${chromaDifference}`);
   return (
-    -4 * chromaDifference * Math.pow(scaleValue, 2) +
-    4 * chromaDifference * scaleValue +
+    -4 * (chromaDifference * Math.pow(scaleValue, 2)) +
+    4 * (chromaDifference * scaleValue) +
     minChroma
   );
 };
 
 /**
  *
- * @param {number} scaleValue Must be between 0 and 1
+ * @param {number} scaleValue - Must be between 0 and 1
  * @returns {number}
  */
 const computeScaleLightness = (scaleValue) => 1 - scaleValue;
 
 /**
- * TODO: Try to work it out, how it will work
- * with a backgroundY variable, to change the Lightness.
  *
  * Channel|Range|Description
  * ---|---------|---
@@ -65,10 +61,10 @@ const computeScaleLightness = (scaleValue) => 1 - scaleValue;
  *
  * @param {number} scaleNumber
  * @param {number} maxScaleNumber
- * @param {number} baseHue
- * @param {number} minChroma Must be between 0 and 1
- * @param {number} maxChroma Must be between 0 and 1
- * @returns {string} The calculated hexcode as a string
+ * @param {number} baseHue - A hue that is between 0 and 360
+ * @param {number} minChroma - Must be between 0 and 1
+ * @param {number} maxChroma - Must be between 0 and 1
+ * @returns {string | Error} - The calculated hexcode as a string
  */
 const computeColorAtScaleNumber = (
   scaleNumber,
@@ -77,13 +73,22 @@ const computeColorAtScaleNumber = (
   minChroma,
   maxChroma
 ) => {
-  if (minChroma > 1 || minChroma < 0 || maxChroma > 1 || minChroma < 0) return;
-  if (minChroma > maxChroma) return;
+  if (scaleNumber > maxScaleNumber)
+    return Error("'scaleNumber' can not be bigger than 'maxScaleNumber'");
+  if (minChroma > 1 || minChroma < 0)
+    return Error("'minChroma' needs to be between 0 and 1");
+  if (maxChroma > 1 || maxChroma < 0)
+    return Error("'maxChroma' needs to be between 0 and 1");
+  if (minChroma > maxChroma)
+    return Error("'minChroma' is not allowed to be bigger than 'maxChroma'");
   const scaleValue = normalizeScaleNumber(scaleNumber, maxScaleNumber);
   const h = computeScaleHue(scaleValue, baseHue);
-  const s = computeScaleChroma(scaleValue, minChroma, maxChroma);
+  const s =
+    Math.round(computeScaleChroma(scaleValue, minChroma, maxChroma) * 100) /
+    100;
   const l = computeScaleLightness(scaleValue);
-  return formatHex(
+  // console.log(`${h}, ${s}, ${l}`);
+  return formatRgb(
     convertOklabToRgb(
       convertOkhslToOklab(parse(`color(--okhsl ${h} ${s} ${l})`))
     )
@@ -100,16 +105,16 @@ class Colour {
    * @returns {Color | Error}
    */
   constructor(name, hue, minChroma = 0, maxChroma = 1) {
-    if (name == undefined) return Error("'name' needs to be defined.");
-    if (typeof name !== "string") return Error("'name' needs to be a string.");
-    if (typeof hue !== "number") return Error("'hue' needs to be anumber.");
-    if (!Number.isInteger(hue)) return Error("'hue' needs to be an integer.");
+    if (name == undefined) throw Error("'name' needs to be defined.");
+    if (typeof name !== "string") throw Error("'name' needs to be a string.");
+    if (typeof hue !== "number") throw Error("'hue' needs to be anumber.");
+    if (!Number.isInteger(hue)) throw Error("'hue' needs to be an integer.");
     if (hue < 0 || hue > 360)
-      return Error("'hue' needs to be between 0 and 360");
+      throw Error("'hue' needs to be between 0 and 360");
     if (minChroma < 0 || minChroma > 1)
-      return Error("'minChroma' needs to between 0 and 1.");
+      throw Error("'minChroma' needs to between 0 and 1.");
     if (maxChroma < 0 || maxChroma > 1)
-      return Error("'maxChroma' needs to between 0 and 1.");
+      throw Error("'maxChroma' needs to between 0 and 1.");
     this.name = name;
     this.hue = hue;
     this.minChroma = minChroma;
@@ -123,7 +128,7 @@ class Colour {
    * @param {number} end
    * @returns {string | Error}
    */
-  generate(steps, start, end) {
+  generate(steps = 10, start = 0, end = 100) {
     if (steps == undefined) return Error("'steps' is undefined.");
     if (start == undefined) return Error("'start' is undefined.");
     if (end == undefined) return Error("'end' is undefined.");
@@ -133,9 +138,9 @@ class Colour {
         i,
         end,
         this.hue,
-        this.minChroma != undefined ? 0 : this.minChroma,
-        this.maxChroma != undefined ? 1 : this.maxChroma
-      )}\n`;
+        this.minChroma,
+        this.maxChroma
+      )};\n`;
     }
     return str;
   }
@@ -146,6 +151,78 @@ const blue = new Colour("blue", 250);
 const green = new Colour("green", 145);
 const red = new Colour("red", 20);
 const yellow = new Colour("yellow", 70);
+const purple = new Colour("purple", 300);
+const orange = new Colour("orange", 30);
 
-const colour_array = [neutral, blue, green, red, yellow];
-colour_array.forEach((e) => console.log(e.generate(10, 0, 100)));
+const start = 0;
+const end = 100;
+const step = 10;
+
+const colour_array = [neutral, blue, green, red, yellow, purple, orange];
+
+let css = ":root {\n";
+colour_array.forEach((e) => (css += e.generate(step, start, end)));
+css += "}\n";
+
+colour_array.forEach((e) => {
+  for (let i = start; i <= end; i = i + step) {
+    css += `.${e.name}-${i} {background-color: var(--${e.name}-${i});}\n`;
+  }
+});
+await Deno.writeTextFile("color.css", css);
+
+const capitalize = (s) => (s && s[0].toUpperCase() + s.slice(1)) || "";
+
+let colors = "<table><thead><tr><th>Scale Number</th>";
+colour_array.forEach((e) => {
+  colors += `<th>${capitalize(e.name)}</th>`;
+});
+colors += "</thead><tbody>";
+for (let i = start; i <= end; i += step) {
+  colors += `<tr><td>${i}</td>`;
+  colour_array.forEach((e) => {
+    colors += `<td><span class="swatch ${e.name}-${i}"><span></td>`;
+  });
+  colors += `</tr>`;
+}
+colors += "</tbody></table>";
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Colours</title>
+  <style>
+  .swatch {
+    border-radius: .25rem;
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, .05);
+    display: inline-block;
+    height: .75em;
+    margin-left: .125em;
+    margin-top: 0;
+    white-space: nowrap;
+    width: .75em;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th {
+  background-color: #04AA6D;
+  color: white;
+}
+td {
+  text-align: center;
+}
+tr:hover {background-color: coral;}
+</style>
+<link rel="stylesheet" href="color.css">
+</head>
+<body>
+${colors}
+</body>
+</html>
+`;
+
+await Deno.writeTextFile("index.html", html);
--- readme.md
+++ readme.md
@@ -1,3 +1,38 @@
 # webdev
 
 A repository full of files with various things.
+
+## colour.js
+
+### Hue Notes:
+
+(Based in an RGB Hue circle with 24 colours a 15°)
+Just usable for HSL/HSV
+
+| Colour         | Degree |
+| -------------- | ------ |
+| red            | 0      |
+| orange red     | 15     |
+| orange         | 30     |
+| orange yellow  | 45     |
+| yellow         | 60     |
+| lemon yellow   | 75     |
+| yellow green   | 90     |
+| sap green      | 115    |
+| green          | 120    |
+| bluish green   | 135    |
+| turquise       | 150    |
+| greenish cyan  | 165    |
+| cyan blue      | 180    |
+| bluish cyan    | 195    |
+| blue           | 210    |
+| blue violet    | 225    |
+| violet         | 240    |
+| purple violet  | 255    |
+| purple         | 270    |
+| purple magenta | 285    |
+| magenta        | 300    |
+| crimson        | 315    |
+| scarlet        | 330    |
+| scarlet red    | 345    |
+| red            | 360    |
